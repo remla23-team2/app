522b9f9fd342229bbb4a1813b187ea81d9e22cb9
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-05-11 20:43:36 +0200

    added lib

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { createStyles, Header, Title } from '@mantine/core';
-
+import { getVersion } from '@remla23-team2/lib2/version-util';
 import './navbar.scss'
 
 export function Navbar() {
     return (
         <Header height={60} mb={120} bg='transparent' >
-
+            <span>Current lib version: {getVersion()} </span>
             {/* <Title c={'brand'} td={'underline'}>Gourmet Groove </Title> */}
 
 
