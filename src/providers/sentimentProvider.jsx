569085ecfdbecd0ced983699b5c114793adda095
Merlijn Mac Gillavry <M.A.P.MacGillavry@student.tudelft.nl>
2023-05-14 19:12:48 +0200

    Update sentimentProvider.jsx

--- src/providers/sentimentProvider.jsx
+++ src/providers/sentimentProvider.jsx
@@ -4,14 +4,14 @@ export const SentimentContext = createContext({})
 
 export default function SentimentProvider(props) {
 
-    async function getSentiment(review) {
+    async function getSentiment(values) {
         const url = "http://localhost:8080/predict"
         const httpOptions = {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ "review": review })
+            body: JSON.stringify({ "review": values.review, "ground_truth": values.ground_truth })
         }
         const response = await fetch(url, httpOptions);
         console.log(response); // log the response object
