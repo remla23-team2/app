15967b972d2ca59a695397f5ef19fe0c2599b3b0
Ivor <i.zagorac@student.tudelft.nl>
2023-05-16 23:55:57 +0200

    Implement using axios for API request

--- src/providers/sentimentProvider.jsx
+++ src/providers/sentimentProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import axios from 'axios'
 
 export const SentimentContext = createContext({})
 
@@ -15,7 +16,8 @@ export default function SentimentProvider(props) {
         }
         console.log(process.env); // log the env object
         console.log(url); // log the url
-        const response = await fetch(url, httpOptions);
+        // const response = await fetch(url, httpOptions);
+        const response = await axios.post(url, { "review": review });
         console.log(response); // log the response object
         const responseBody = await response.json();
         console.log(responseBody); // log the response body
